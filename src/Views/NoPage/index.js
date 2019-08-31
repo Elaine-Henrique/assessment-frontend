e2433683446b02d85328d15048d5a86f803bef59
Elaine <silva.elainehenrique@gmail.com>
2019-08-30 23:06:54 -0300

    NoPAge-responsiva

--- src/Views/NoPage/index.js
+++ src/Views/NoPage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles.css';
 
 import Navbar from '../../Components/Navbar';
 
 const NoPage = () => (
 	<div>
 		<Navbar />
-		<img src='https://i.imgur.com/aDwTQV9.png' width="600" height="530" style={{ display: 'block', margin: 'auto' }} alt='404 Error'/>'
+		<img src='https://i.imgur.com/aDwTQV9.png' className="pageError" alt='404 Error'/>'
 	</div>
 )
 
